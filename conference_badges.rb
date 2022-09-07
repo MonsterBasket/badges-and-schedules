40704c8ff668cff441b9693b1c0d4131319e1273
MonsterBasket <blazza1@hotmail.com>
2022-09-07 22:47:20 +1000

    done

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker (name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (names)
+  names.collect {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms (names)
+  new_array = []
+  names.each_with_index {|name, index| new_array.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")}
+  new_array
+end
+
+def printer (names)
+  badges = batch_badge_creator names
+  rooms = assign_rooms names
+  badges.each {|badge| puts badge}
+  rooms.each {|room| puts room}
+end
